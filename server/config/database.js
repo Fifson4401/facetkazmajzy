54670cc59ffc91eb41501bd5dd0f70f5ab001a2e
Filip Wareluk <filip.wareluk@revolve.pro>
2024-08-18 00:20:35 +0200

    Rollback to strapi v4

--- server/config/database.js
+++ server/config/database.js
@@ -1,13 +1,13 @@
-import path from 'path';
+const path = require('path');
 
-export default ({ env }) => {
+module.exports = ({ env }) => {
   const client = env('DATABASE_CLIENT', 'postgres');
 
   const connections = {
     postgres: {
       connection: {
         connectionString: env('DATABASE_URL'),
-        host: env('DATABASE_HOST', '127.0.0.1'),
+        host: env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'strapi'),
         user: env('DATABASE_USERNAME', 'strapi'),
