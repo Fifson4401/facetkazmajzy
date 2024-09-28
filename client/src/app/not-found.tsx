be5c11784773bc9e5bded6c66ba129debea59144
Filip Wareluk <filip.wareluk@revolve.pro>
2024-09-28 12:39:19 +0200

    Get menu on 404

--- client/src/app/not-found.tsx
+++ client/src/app/not-found.tsx
@@ -1,11 +1,14 @@
+import { getHeaderMenuProps } from "@/api/pages/getPageProps";
 import Footer from "@/core/components/Footer/Footer";
 import Header from "@/core/components/Header/Header";
 import { Button, Link, Image } from "@nextui-org/react";
 
-export default function NotFound() {
+export default async function NotFound() {
+  const { menu } = await getHeaderMenuProps()
+
   return (
     <div className='flex flex-col min-h-screen justify-between'>
-      <Header />
+      <Header menu={menu} />
       <main
         lang={'pl'}
         className="flex-1 flex flex-col items-center gap-6 overflow-hidden bg-background p-8 text-foreground light md:gap-10 md:px-14 md:py-8 xl:px-28"
