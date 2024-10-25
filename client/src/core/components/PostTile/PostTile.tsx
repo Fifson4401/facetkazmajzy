0f169ed2d68a3b971af4889fb52e79ee52bc4f5c
Filip Wareluk <filip.wareluk@revolve.pro>
2024-10-25 22:55:15 +0200

    Types fixes

--- client/src/core/components/PostTile/PostTile.tsx
+++ client/src/core/components/PostTile/PostTile.tsx
@@ -5,7 +5,7 @@ import { useRouter } from 'next/navigation';
 import CategoryChip from '../CategoryChip/CategoryChip';
 import { PostTileProps } from '@/api/interfaces/collections/blogPosts';
 import TagsLink from '../TagsLink/TagsLink';
-import { parseContent } from '../PostContentRenderer/PostContentRenderer';
+import PostContentRenderer from '../PostContentRenderer/PostContentRenderer';
 
 const PostTile: FC<PostTileProps> = ({
   category,
@@ -17,7 +17,6 @@ const PostTile: FC<PostTileProps> = ({
 }) => {
   const router = useRouter();
 
-  const parsedElements = description ? parseContent(description) : '';
   return (
     <div
       onClick={() => router.push(`zadania/${slug}`)}
@@ -31,7 +30,7 @@ const PostTile: FC<PostTileProps> = ({
         <CategoryChip {...sub_category} />
       </div>
       <p className="line-clamp-2 pt-4 text-sm text-[#2b2b2b] md:text-base">
-        {parsedElements}
+        <PostContentRenderer content={description} />
       </p>
       <div className="flex flex-row gap-2 pt-3">
         <TagsLink {...tags} />
