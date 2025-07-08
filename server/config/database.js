e527d0ddf6c4087b5519c260559cd58aa151eb51
Filip Wareluk <filip.wareluk2@gmail.com>
2025-07-08 23:10:28 +0200

    hope the last fix

--- server/config/database.js
+++ server/config/database.js
@@ -1,21 +1,14 @@
-module.exports = ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'postgres');
-  const host = env('DATABASE_HOST', '127.0.0.1');
-
-  return {
+module.exports = ({ env }) => ({
+  connection: {
+    client: env('DATABASE_CLIENT', 'postgres'),
     connection: {
-      client,
-      connection: {
-        host,
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: {
-          rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
-        },
-      },
-      debug: false,
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'strapi'),
+      user: env('DATABASE_USERNAME', 'strapi'),
+      password: env('DATABASE_PASSWORD', 'strapi'),
+      ssl: false,
     },
-  };
-};
+    debug: false,
+  },
+});
