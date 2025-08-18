42f2d813e44c1a57da8316e43424b5c52a122e6c
Filip Wareluk <filip.wareluk2@gmail.com>
2025-08-18 19:20:54 +0200

    Error fixes

--- client/src/features/error/ErrorBoundary.tsx
+++ client/src/features/error/ErrorBoundary.tsx
@@ -19,20 +19,20 @@ interface ErrorBoundaryState {
 
 const doodles: Doodle[] = [
   {
-    name: 'exclamation-mark-1',
-    width: 50,
-    height: 50,
-    bottom: 20,
-    left: 20,
-    transform: 'rotate(-15deg)',
+    src: 'exclamation-mark-1',
+    width: '50',
+    height: '50',
+    bottom: '20',
+    left: '20',
+    rotate: -15,
   },
   {
-    name: 'exclamation-mark-2',
-    width: 30,
-    height: 30,
-    top: 20,
-    right: 20,
-    transform: 'rotate(15deg)',
+    src: 'exclamation-mark-2',
+    width: '30',
+    height: '30',
+    top: '20',
+    right: '20',
+    rotate: 15,
   },
 ];
 
@@ -54,16 +54,15 @@ class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
   render() {
     if (this.state.hasError) {
       return (
-        <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100 p-4 text-center">
+        <div className="flex min-h-screen flex-col items-center justify-center  p-4 text-center">
           <DoodleContainer doodles={doodles}>
-            <div className="max-w-lg rounded-lg bg-white p-8 shadow-lg">
-              <Header level={1} withHighlighter>
-                Ups! Coś poszło nie tak.
-              </Header>
+            <div className="max-w-lg rounded-lg p-8 shadow-lg">
+              <Header level={1} withHighlighter text='Ups! Coś poszło nie tak.'/>
+                
               <Text className="my-4">
                 Przepraszamy za niedogodności. Nasi technicy zostali poinformowani i pracują nad rozwiązaniem problemu.
               </Text>
-              <Button onClick={() => (window.location.href = '/')}>
+              <Button href={'/'}>
                 Wróć na stronę główną
               </Button>
               {process.env.NODE_ENV === 'development' &&
