888211e21d05dccf914d07e2f0f44e20921f65b9
Filip Wareluk <filip.wareluk2@gmail.com>
2025-08-21 00:16:14 +0200

    last call x3

--- server/deployment.sh
+++ server/deployment.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
-# Zatrzymaj skrypt, jeśli którekolwiek polecenie zawiedzie
-set -e
 
-echo ">>> Przechodzenie do katalogu aplikacji..."
 cd ~/domains/api.facetkazmajzy.pl/public_nodejs/ || exit
 
-echo ">>> Czyszczenie starych modułów i artefaktów..."
-rm -rf node_modules build .cache
+echo ">>> Instalowanie zależności z flagami dla FreeBSD..."
 
-echo ">>> Instalowanie wszystkich zależności (npm install)..."
-# Używamy Twojego aliasu npm20
-npm20 install
-
-echo ">>> Budowanie aplikacji Strapi (npm run build)..."
-npm20 run build
+npm20 install --platform=freebsd --arch=x64 --ignore-scripts=false
 
 echo ">>> Restartowanie aplikacji..."
 devil www restart api.facetkazmajzy.pl
