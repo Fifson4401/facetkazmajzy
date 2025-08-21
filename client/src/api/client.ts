31274a0457fc127b540ebd613a1d452f2a85d46e
Filip Wareluk <filip.wareluk2@gmail.com>
2025-08-21 12:45:56 +0200

    client fix

--- client/src/api/client.ts
+++ client/src/api/client.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const client = axios.create({ baseURL: process.env.API_HOST });
+const client = axios.create({ baseURL: process.env.NEXT_PUBLIC_API_HOST });
 client.defaults.headers.common['Authorization'] =
   `Bearer ${process.env.API_TOKEN}`;
 client.defaults.headers.common['Strapi-Response-Format'] =
