47f54ace12fea589a8c31600b6cdb61fcb2b282f
Filip Wareluk <filip.wareluk2@gmail.com>
2025-08-21 09:39:21 +0200

    database config change

--- server/config/database.js
+++ server/config/database.js
@@ -1,14 +1,23 @@
-export default ({ env }) => ({
-  connection: {
-    client: env('DATABASE_CLIENT', 'postgres'),
+module.exports = ({ env }) => {
+  console.log('Loading database config...');
+  console.log('DATABASE_CLIENT:', env('DATABASE_CLIENT'));
+  console.log('DATABASE_HOST:', env('DATABASE_HOST'));
+  
+  const config = {
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'strapi'),
-      password: env('DATABASE_PASSWORD', 'strapi'),
-      ssl: env.bool('DATABASE_SSL', false), // Użycie env.bool() jest bezpieczniejsze
+      client: env('DATABASE_CLIENT', 'postgres'),
+      connection: {
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        user: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi'),
+        ssl: env.bool('DATABASE_SSL', false),
+      },
+      debug: false,
     },
-    debug: false,
-  },
-});
+  };
+  
+  console.log('Database config created successfully');
+  return config;
+};
