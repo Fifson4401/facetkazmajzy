f32b813196c89841bb83bb8e24aa9edf5b511407
Filip Wareluk <filip.wareluk2@gmail.com>
2025-08-21 13:10:59 +0200

    config gix

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -6,12 +6,13 @@ const nextConfig = {
   },
   images: {
     remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'api.facetkazmajzy.pl',
-        port: '',
+       {
+        protocol: process.env.NODE_ENV === 'production' ? 'https' : 'http',
+        hostname: process.env.NODE_ENV === 'production'
+          ? 'api.facetkazmajzy.pl'
+          : 'localhost',
         pathname: '/uploads/**', // Allows any image from the /uploads/ path
-      },
+      }
     ],
     deviceSizes: [320, 375, 575, 768, 1000, 1200, 1999],
     minimumCacheTTL: 86400000, // 1 day
